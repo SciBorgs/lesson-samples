57d7dbc4de58598ad06cb1d70a5fd63387dc47ac
Dhinak G <17605561+dhinakg@users.noreply.github.com>
2021-04-17 13:10:51 -0400

    Add immutable strings, make folder, add .vscode launch

---
+++ Week 2/ImmutableStrings.java
@@ -0,0 +1,17 @@
+/**
+ * ImmutableStrings
+ * 
+ * Quick example on how strings are immutable.
+ */
+public class ImmutableStrings {
+    public static void main(String[] args) {
+        String pieType = "pie";
+        System.out.println("Original pieType: " + pieType); // This function prints out the value of pieType, which is
+                                                            // pie
+
+        String newPieType = pieType.toUpperCase(); // This doesn't modify the existing string, as we'll see below
+
+        System.out.println("pieType after toUpperCase: " + pieType); // Did not change!
+        System.out.println("newPieType: " + newPieType); // Our newPieType variable has the uppercased pieType
+    }
+}
